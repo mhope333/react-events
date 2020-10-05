bf08c6351f406b193297ff4423fdeb4b73050609
Hope <hopem@bglm33091.b2b.regn.net>
2020-10-05 15:58:56 +0100

    Updates

---
+++ precache-manifest.ceb24bf14972f91dc29080301924825b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a344833b60117553e060e383490630c1",
+    "url": "https://mhope333.github.io/index.html"
+  },
+  {
+    "revision": "5e3c60e7f508479a1deb",
+    "url": "https://mhope333.github.io/static/css/main.b624c85d.chunk.css"
+  },
+  {
+    "revision": "6bdb1ef6262623297118",
+    "url": "https://mhope333.github.io/static/js/2.37934857.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "https://mhope333.github.io/static/js/2.37934857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3c60e7f508479a1deb",
+    "url": "https://mhope333.github.io/static/js/main.95829252.chunk.js"
+  },
+  {
+    "revision": "b514185b96fc4d2b8e97",
+    "url": "https://mhope333.github.io/static/js/runtime-main.36c6105d.js"
+  },
+  {
+    "revision": "ecaeec0fa9a4b9276b7826068e609d7d",
+    "url": "https://mhope333.github.io/static/media/conference-stock-image.ecaeec0f.jpg"
+  },
+  {
+    "revision": "9e582d1277e2cbfd7731ab29572665db",
+    "url": "https://mhope333.github.io/static/media/marketingIcon.9e582d12.png"
+  },
+  {
+    "revision": "7cc47bab0132669e448a0fd5559782c4",
+    "url": "https://mhope333.github.io/static/media/seminarIcon.7cc47bab.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://mhope333.github.io/precache-manifest.ef37a9670ac103821df22bfba231b4e2.js"
+  "https://mhope333.github.io/precache-manifest.ceb24bf14972f91dc29080301924825b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://mhope333.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://mhope333.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
