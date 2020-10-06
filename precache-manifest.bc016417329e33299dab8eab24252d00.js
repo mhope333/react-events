41d60c4bc982c948a99cb144c5f22ed37db29ee6
Hope <hopem@bglm33091.b2b.regn.net>
2020-10-06 08:21:07 +0100

    Updates

---
+++ precache-manifest.bc016417329e33299dab8eab24252d00.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5889e3b9a166dc0374c0b4764b0c767e",
+    "url": "/react-events/index.html"
+  },
+  {
+    "revision": "b65a37f0bccaa481f21e",
+    "url": "/react-events/static/css/main.bc96744f.chunk.css"
+  },
+  {
+    "revision": "6bdb1ef6262623297118",
+    "url": "/react-events/static/js/2.37934857.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/react-events/static/js/2.37934857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65a37f0bccaa481f21e",
+    "url": "/react-events/static/js/main.0815f0ac.chunk.js"
+  },
+  {
+    "revision": "8557cab3564a19a6c2c5",
+    "url": "/react-events/static/js/runtime-main.b39cfac6.js"
+  },
+  {
+    "revision": "ecaeec0fa9a4b9276b7826068e609d7d",
+    "url": "/react-events/static/media/conference-stock-image.ecaeec0f.jpg"
+  },
+  {
+    "revision": "9e582d1277e2cbfd7731ab29572665db",
+    "url": "/react-events/static/media/marketingIcon.9e582d12.png"
+  },
+  {
+    "revision": "7cc47bab0132669e448a0fd5559782c4",
+    "url": "/react-events/static/media/seminarIcon.7cc47bab.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://mhope333.github.io/precache-manifest.ceb24bf14972f91dc29080301924825b.js"
+  "/react-events/precache-manifest.bc016417329e33299dab8eab24252d00.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://mhope333.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-events/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
